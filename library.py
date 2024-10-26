b7d34084703f5a445d653f59b5a266b00d760df6
faruklmu17 <41293321+faruklmu17@users.noreply.github.com>
2024-10-26 10:41:58 -0400

    Update library.py
    added a list of books

--- library.py
+++ library.py
@@ -1,1 +1,11 @@
+available_books = [
 
+"The Fault in Our Stars", "The Hunger Games", "To All the Boys I've Loved Before",
+
+"Harry Potter and the Sorcerer's Stone", "The Perks of Being a Wallflower",
+
+"Looking for Alaska", "The Hate U Give", "The Maze Runner",
+
+"Divergent", "Eleanor & Park"
+
+]
